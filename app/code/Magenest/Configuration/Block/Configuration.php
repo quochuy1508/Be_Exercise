4586fd42372fd3d928ee9833cbf20a605712f96c
Leo Ho <huyhq@magenest.com>
2021-09-30 14:10:56 +0700

    Exercise 1 Custom config

--- app/code/Magenest/Configuration/Block/Configuration.php
+++ app/code/Magenest/Configuration/Block/Configuration.php
@@ -2,10 +2,95 @@
 
 namespace Magenest\Configuration\Block;
 
-class Configuration extends \Magento\Framework\View\Element\Template
+use Magento\Framework\Acl\AclResource\TreeBuilder;
+use Magento\Framework\Serialize\Serializer\Json;
+use Magento\Framework\View\Element\Template;
+use Magenest\Configuration\Model\ConfigFactory;
+use Magenest\Configuration\Model\Config;
+use Magento\Integration\Helper\Data;
+
+class Configuration extends Template
 {
+    /**
+     * @var string
+     */
+    protected $_template = 'Magenest_Configuration::custom_configuration.phtml';
+
+    /**
+     * @var ConfigFactory
+     */
+    private $configFactory;
+
+    /**
+     * @var Json
+     */
+    protected $encoder;
+
+    /**
+     * @var TreeBuilder
+     */
+    protected $_resourceTreeBuilder;
+
+    /**
+     * @var Data
+     */
+    protected $_integrationData;
+
+    public function __construct(
+        Template\Context $context,
+        ConfigFactory $configFactory,
+        Json $encoder,
+        TreeBuilder $resourceTreeBuilder,
+        Data $integrationData,
+        array $data = []
+    ) {
+        $this->configFactory = $configFactory;
+        $this->encoder = $encoder;
+        $this->_resourceTreeBuilder = $resourceTreeBuilder;
+        $this->_integrationData = $integrationData;
+        parent::__construct($context, $data);
+    }
+
     public function _prepareLayout()
     {
         return parent::_prepareLayout();
+    }
+
+    /**
+     * Get Json Representation of Resource Tree
+     *
+     * @return string
+     */
+    public function getTree()
+    {
+        $aclResourcesTree =  $this->_integrationData->mapResources($this->getAclResources());
+        return $this->encoder->serialize($aclResourcesTree);
+    }
+
+    /**
+     * Get lit of all ACL resources declared in the system.
+     *
+     * @return array
+     */
+    public function getAclResources()
+    {
+        /** @var Config $config */
+        $config = $this->configFactory->create();
+        $resources = $config->get();
+        if (!empty($resources['config']['acl']['resources'])) {
+            $tree = $this->_resourceTreeBuilder->build($resources['config']['acl']['resources']);
+        } else {
+            $tree = [];
+        }
+
+        $configResource = array_filter(
+            $tree,
+            function ($node) {
+                return isset($node['id'])
+                    && $node['id'] == 'Magento_Frontend::customer';
+            }
+        );
+        $configResource = reset($configResource);
+        return $configResource['children'] ?? [];
     }
 }
--- app/code/Magenest/Configuration/Model/Config/Converter.php
+++ app/code/Magenest/Configuration/Model/Config/Converter.php
@@ -2,25 +2,67 @@
 
 namespace Magenest\Configuration\Model\Config;
 
+/**
+ * @inheritDoc
+ */
 class Converter implements \Magento\Framework\Config\ConverterInterface
 {
+    /**
+     * @inheritdoc
+     *
+     * @param \DOMDocument $source
+     * @return array
+     * @throws \Exception
+     */
     public function convert($source)
     {
-        $resources = $source->getElementsByTagName('resources');
-        $discountDetails = [];
-        $iterator = 0;
-
-        /** @var \DOMElement $resource */
-        foreach ($resources as $resource) {
-            /** @var \DOMText $resourceInfo */
-            foreach ($resource->childNodes as $resourceInfo) {
-                if ($resourceInfo->nodeName && $resourceInfo->nodeName !== "#text") {
-                    $discountDetails[$resource->getAttribute('title')][$resourceInfo->nodeName] = $resourceInfo->textContent;
-                }
-            }
-            $iterator++;
+        $aclResourceConfig = ['config' => ['acl' => ['resources' => []]]];
+        $xpath = new \DOMXPath($source);
+        /** @var $resourceNode \DOMNode */
+        foreach ($xpath->query('/config/acl/resources/resource') as $resourceNode) {
+            $aclResourceConfig['config']['acl']['resources'][] = $this->_convertResourceNode($resourceNode);
         }
+        return $aclResourceConfig;
+    }
 
-        return ['resources' => $discountDetails];
+    /**
+     * Convert resource node into assoc array
+     *
+     * @param \DOMNode $resourceNode
+     * @return array
+     * @throws \Exception
+     * @SuppressWarnings(PHPMD.NPathComplexity)
+     */
+    protected function _convertResourceNode(\DOMNode $resourceNode)
+    {
+        $resourceData = [];
+        $resourceAttributes = $resourceNode->attributes;
+        $idNode = $resourceAttributes->getNamedItem('id');
+        if ($idNode === null) {
+            // phpcs:ignore Magento2.Exceptions.DirectThrow
+            throw new \Exception('Attribute "id" is required for ACL resource.');
+        }
+        $resourceData['id'] = $idNode->nodeValue;
+        $moduleNode = $resourceAttributes->getNamedItem('module');
+        if ($moduleNode !== null) {
+            $resourceData['module'] = $moduleNode->nodeValue;
+        }
+        $titleNode = $resourceAttributes->getNamedItem('title');
+        if ($titleNode !== null) {
+            $resourceData['title'] = $titleNode->nodeValue;
+        }
+        $sortOrderNode = $resourceAttributes->getNamedItem('sortOrder');
+        $resourceData['sortOrder'] = $sortOrderNode !== null ? (int)$sortOrderNode->nodeValue : 0;
+        $disabledNode = $resourceAttributes->getNamedItem('disabled');
+        $resourceData['disabled'] = $disabledNode !== null && $disabledNode->nodeValue == 'true';
+        // convert child resource nodes if needed
+        $resourceData['children'] = [];
+        /** @var $childNode \DOMNode */
+        foreach ($resourceNode->childNodes as $childNode) {
+            if ($childNode->nodeName == 'resource') {
+                $resourceData['children'][] = $this->_convertResourceNode($childNode);
+            }
+        }
+        return $resourceData;
     }
 }
